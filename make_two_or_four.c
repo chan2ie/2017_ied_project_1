2e0f17c837381cb59e8170f648f0b40fd592b169
chan2ie <dlcksgml17@u.sogang.ac.kr>
2017-05-18 19:41:11 -0700

    v 2.0 ~ return

--- make_two_or_four.c
+++ make_two_or_four.c
@@ -8,18 +8,37 @@
 int main(void) {
 	
 	int b[4][4] = {0};
-
-	printf("%d\n",b[0][0] == 0);
+	int b1, b2;
+	int i, j, sum = 0;
+	
+	for(i = 0; i < 4; i++){
+		for(j = 0; j < 4; j++){
+			if (b[i][j] != 0){
+				sum += 1;
+			}
+		}
+	}
 
 	srand(time(NULL));
 
-	if(rand() % 3 == 0){
-		b[rand() % 4][rand() % 4] = 4;
-	}
-	else{
-		b[rand() % 4][rand() % 4] = 2;
-	}
+	b1 = rand() % 4;
+	b2 = rand() % 4;
 
+	if(sum == 16){
+		return 0;
+	}
+	if(b[b2][b2] == 0){
+		if(rand() % 3 == 0){
+			b[b1][b2] = 4;
+
+			return 4;
+		}
+		else{
+			b[b1][b2] = 2;
+
+			return 2;
+		}
+	}
 
 	printf("%d %d %d %d\n",b[0][0],b[0][1],b[0][2],b[0][3]);
 	printf("%d %d %d %d\n",b[1][0],b[1][1],b[1][2],b[1][3]);
